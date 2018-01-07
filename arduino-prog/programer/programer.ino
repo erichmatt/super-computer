ad89ea23c3eb37658d73052ee355082e6975e8f0
Eric <erichmatt@gmail.com>
2018-01-07 11:58:17 -0500

    added a basic program to output binary

--- arduino-prog/programer/programer.ino
+++ arduino-prog/programer/programer.ino
@@ -1,5 +1,10 @@
 void setup() {
   // put your setup code here, to run once:
+   pinMode(14, OUTPUT);
+   pinMode(13, OUTPUT);
+   pinMode(12, OUTPUT);
+   pinMode(11, OUTPUT);
+   pinMode(10, OUTPUT);
 
 }
 void loop() {
@@ -10,8 +15,8 @@ binout(0);
 
 void binout(int var)
 {
-   digitalWrite(11, HIGH && (var & B00001000));
-   digitalWrite(12, HIGH && (var & B00000100));
-   digitalWrite(13, HIGH && (var & B00000010));
-   digitalWrite(14, HIGH && (var & B00000001));
+   digitalWrite(10, HIGH && (var & B00001000));
+   digitalWrite(11, HIGH && (var & B00000100));
+   digitalWrite(12, HIGH && (var & B00000010));
+   digitalWrite(13, HIGH && (var & B00000001));
 }
