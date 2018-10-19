6443dbc0b9508f13ae3f59fdfa7c429fc573b382
Eric Matt <erichmatt@gmail.com>
2018-10-19 19:13:35 -0400

    Added comments and fixed some bugs hopefully it works

--- programs/bubble.asm
+++ programs/bubble.asm
@@ -14,7 +14,8 @@
 	plo 4h		; |
 	ldi high tbs	; |Set Register 4 as list pointer
 	phi 4h		;/
-	seq
+
+	seq		;Set Q to show the list needs to be sorted
 
 exitd	sep 3h		;Set Register 3 as program counter to return to main program
 delay	ldi 20h		;\
@@ -34,33 +35,31 @@ main	ldi 0ah		;\
 sort	dec 05h
 	glo 05h
 	bz main
-	ldx
-	out 4h		; Display contense of memory at address X
+	out 4h		; Display memory at address X
 	sep 0Eh		; Go to Delay
-
+	ldx
 	irx		;\Increment register X
 	sm		;/and check to see if the next item in the list is bigger
 	bnf sort
-	seq
-	ldx
-	phi 5
-	dec 4
-	ldxa
-	stxd
-	ghi 5
-	str 4
-	dec 5
-	glo 5
-	bnz sort
-   	nop
+
+	seq		;Set Q to show a swap was needed
+
+	ldx		;\
+	phi 5		;/Store 2nd item in the high byte of reg 5
+	dec 4		;Point X to 1st location
+	ldxa		;Load D with 1st item and point to 2nd location
+	stxd		;Store 1st in 2nd location and point to 1st location
+	ghi 5		;Get 2nd item from high byte of reg 5
+	str 4		;Store 2nd item in first location
+   	irx		;point X to second location
     	nop
     	nop
-	br main
-done	seq
-	sep 0eh
-	req
-	sep 0eh
-	br done
+	br sort		;Go through sort loop again
+done	seq		;\
+	sep 0eh		; |
+	req		; |Flash Q to show list is sorted.
+	sep 0eh		; |
+	br done		;/
 
 tbs	byte 10h
 	byte 23h
